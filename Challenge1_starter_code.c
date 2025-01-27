c72f9d75ba68aae104d5e0c4bc09b8a11c37e879
bhagyeshagresar <bhagyeshagresar@gmail.com>
2025-01-26 21:35:38 -0600

    added the alarm handler

--- Challenge1_starter_code.c
+++ Challenge1_starter_code.c
@@ -8,7 +8,7 @@
 
 //Function that handles a SIGINT signal
 void handler_sigint(int signum);
-
+void handler_sigalarm(int signum);
 
 int score = 0;
 
@@ -28,11 +28,15 @@ int main() {
 
    srand (time(0));
 
+   alarm(5);
+
    //Function Pointer
    void(*sigHandlerReturn)(int);
+   //void(*alarmSigHandlerReturn)(int);
 
    //Use the signal function to catch the signals
    sigHandlerReturn = signal(SIGINT, handler_sigint);
+   sigHandlerReturn = signal(SIGALRM, handler_sigalarm);
 
    if(sigHandlerReturn == SIG_ERR){
       perror("Signal Error: ");
@@ -64,6 +68,21 @@ void handler_sigint(int signum){
 
    if(signum == SIGINT){
       printf("Received SIGINT Error or Ctrl - C signal\n");
+      exit(0);
+   }
+   else{
+      printf("Received %d Signal\n", signum);
+   }
+
+
+}
+
+
+void handler_sigalarm(int signum){
+
+
+   if(signum == SIGALRM){
+      printf("Received the 5 sec timeout signal\n");
       exit(0);
    }
    else{
